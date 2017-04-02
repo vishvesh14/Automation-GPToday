ad81371d9d5818d604ba23b91046872be2aea2ed
Vishvesh14 <vishveshsavant@gmail.com>
2017-04-02 20:29:29 +0530

    First commit

---
+++ Automation/src/com/testcases/LoginVerification.java
@@ -0,0 +1,14 @@
+package com.testcases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+public class LoginVerification {
+  @Test
+  public void f() {
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+}
